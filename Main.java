8ce516b574af2f893e3deca9a12520e42659376c
Adrienne Davi <adrienne.davi@patelhs.org>
2020-10-08 16:01:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import org.graalvm.compiler.replacements.SnippetLowerableMemoryNode;
+
+class Main {
+  public static void main(String[] args) {
+    
+    boolean snowing= true;
+    boolean hailing= false;
+          
+  }
+}
